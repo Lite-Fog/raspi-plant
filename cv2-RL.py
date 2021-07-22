5b182c1a0471f18f009ba36cf3e745bceba62b0e
Itai <cohitai@gmail.com>
2021-07-22 11:22:15 +0200

    __name__== __main__, new recording

--- cv2-RL.py
+++ cv2-RL.py
@@ -1,27 +1,22 @@
 import cv2
 import requests
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 import glob
 
-PATH_to_stream = 'http://192.168.11.115:8080/?action=streaming'
-PATH_to_data_directory = '/data'
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 
 def delete_data(path):
+    """function deletes data library"""
     files = glob.glob(path + '/*')
     for f in files:
         os.remove(f)
 
 
-delete_data(dir_path + PATH_to_data_directory)
+def record_video(length_secs, path_to_stream, path_to_wd, path_to_data):
+    """function records video from <stream> of length <length_secs>"""
 
-
-def record_video(length_secs):
-
-    r = requests.get(PATH_to_stream, stream=True)
+    r = requests.get(path_to_stream, stream=True)
     if r.status_code == 200:
         bytes_loc = bytes()
         time_start = datetime.now()
@@ -36,12 +31,29 @@ def record_video(length_secs):
                 i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 datetimeobj = datetime.now()  # get time stamp
                 cv2.imshow('img', i)
-                cv2.imwrite(dir_path + PATH_to_data_directory + '/img' + str(datetimeobj) + '.jpg', i)
+                cv2.imwrite(path_to_wd + path_to_data + '/img' + str(datetimeobj) + '.jpg', i)
                 if cv2.waitKey(1) == 27 or (datetimeobj -time_start).seconds > length_secs:  # if user  hit esc
                     exit(0)  # exit program
     else:
         print("Received unexpected status code {}".format(r.status_code))
 
 
-record_video(30)
+#record_video(30)
 
+
+def main():
+
+    # set parameters
+    path_to_stream = 'http://192.168.11.115:8080/?action=streaming'
+    path_to_data_directory = '/data'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    delete_data(dir_path + path_to_data_directory)
+    record_video(30, path_to_stream, dir_path, path_to_data_directory)
+
+    return 0
+
+
+if __name__ == "__main__":
+
+    main()
