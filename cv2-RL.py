e938bf9b5b6218be51728e3be6da62e9165b613d
Itai <cohitai@gmail.com>
2021-07-22 17:01:35 +0200

    record + mask done

--- cv2-RL.py
+++ cv2-RL.py
@@ -4,17 +4,22 @@ import numpy as np
 from datetime import datetime
 import os
 import glob
+from pathlib import Path
 
 
 def delete_data(path):
-    """function deletes data library"""
+    """ function deletes data library"""
     files = glob.glob(path + '/*')
     for f in files:
         os.remove(f)
 
 
-def record_video(length_secs, path_to_stream, path_to_wd, path_to_data):
-    """function records video from <stream> of length <length_secs>"""
+def record_video(length_secs, path_to_stream, path_to_data):
+    """ function records video
+     :param: length_secs: int
+             path_to_stream: string
+             path_to_data: string
+     """
 
     r = requests.get(path_to_stream, stream=True)
     if r.status_code == 200:
@@ -28,32 +33,73 @@ def record_video(length_secs, path_to_stream, path_to_wd, path_to_data):
                 jpg = bytes_loc[a:b+2]  # actual image
                 bytes_loc = bytes_loc[b+2:]  # other information
                 # decode to colored image ( another option is cv2.IMREAD_GRAYSCALE)
-                i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                i = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 datetimeobj = datetime.now()  # get time stamp
-                cv2.imshow('img', i)
-                cv2.imwrite(path_to_wd + path_to_data + '/img' + str(datetimeobj) + '.jpg', i)
-                if cv2.waitKey(1) == 27 or (datetimeobj -time_start).seconds > length_secs:  # if user  hit esc
-                    exit(0)  # exit program
+                #cv2.imshow('img', i)
+                cv2.imwrite(path_to_data + '/img' + str(datetimeobj) + '.jpg', i)
+                if cv2.waitKey(1) == 27 or (datetimeobj - time_start).seconds > length_secs:  # if user  hit esc
+                    break  # exit program
     else:
         print("Received unexpected status code {}".format(r.status_code))
 
 
-#record_video(30)
+def mask_img(img_path, output_lib, sbool=False, output_ext=".jpeg"):
+    """:param img_path: string, path to the image.
+       :param output_lib: string, path to output directory.
+       :param sbool: boolean, if True it will save the masked image, default False.
+       :param output_ext: string, type of output image, default jpeg.
+       :returns np.arrays of green and mask_g
+       """
+
+    img_name = os.path.splitext(Path(img_path).name)[0]
+
+    # output names
+    green_img_path = output_lib + '/' + 'gr_' + img_name + output_ext  # output green img path
+
+    img = cv2.imread(img_path)[:, 150:1050]  # cv2 read
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # convert to hsv
+    mask = cv2.inRange(hsv, (35, 25, 25), (80, 255, 255))  # mask by green spectrum
+
+    # slice out the green
+    imask = mask > 0
+    green = np.zeros_like(img, np.uint8)
+    mask_g = np.zeros_like(img, np.uint8)
+
+    # make white background
+    green.fill(255)
+
+    green[imask] = img[imask]
+    mask_g[~imask] = img[~ imask]
+
+    if sbool:  # save to directory if sbool == True
+        cv2.imwrite(green_img_path, green)
+        print(green_img_path)
+
+    return green, mask_g
+
+
+def convert_images_to_masked(input_dir, output_dir, mask_function):
+
+    input_files_names = glob.glob(input_dir + '/*')
+    print(len(input_files_names))
+    for file in input_files_names:
+        mask_function(file, output_lib=output_dir, sbool=True)
 
 
 def main():
 
     # set parameters
     path_to_stream = 'http://192.168.11.115:8080/?action=streaming'
-    path_to_data_directory = '/data'
+
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    path_to_data_directory = dir_path + '/data'
+    path_to_masked_data_directory = dir_path + '/data-m'
 
-    delete_data(dir_path + path_to_data_directory)
-    record_video(30, path_to_stream, dir_path, path_to_data_directory)
-
-    return 0
+    delete_data(path_to_data_directory)
+    delete_data(path_to_masked_data_directory)
+    record_video(30, path_to_stream, path_to_data_directory)
+    convert_images_to_masked(path_to_data_directory, path_to_masked_data_directory, mask_img)
 
 
 if __name__ == "__main__":
-
     main()
