ae215e1bb668b9c6f994d6bcb2f6d3e42c0f57ee
cohitai <cohitai@gmail.com>
2021-08-23 17:59:27 +0200

    Checkpoint: code reweitten and colmap testing: OK
    pre Poisson test

--- cv2-RL.py
+++ cv2-RL.py
@@ -1,3 +1,5 @@
+""" Raspi-plant interacting module """
+
 import cv2
 import requests
 import numpy as np
@@ -11,13 +13,36 @@ import re
 import paramiko
 import time
 import argparse
-# from PIL import Image
-
-# connect to docker daemon
 import docker
 
+# parser
+parser = argparse.ArgumentParser(description="Raspi-plant")
+parser.add_argument("-rec", "--record", default=28, help="make a video", type=int)
+parser.add_argument("-deb", "--debug", default=False, help="end streaming", type=bool)
+parser.add_argument("-col", "--colmap", default=True, help="run colmap from docker container", type=bool)
+parser.add_argument("-dele", "--delete", default=False, help="delete working directory", type=bool)
+parser.add_argument("-mask", "--masking", default=True, help="mask images", type=bool)
 
-""" Raspi-plant interacting module """
+
+FLAGS = parser.parse_args()
+
+# broadcasting Global
+RASPI_USER = "pi"
+RASPI_PI_WIFI = "192.168.11.115"
+RASPI_PI_ETHERNET = "10.150.180.52"
+RASPI_PWD = 'Mancave3090!'
+RASPI_BROADCAST_PORT = 8085
+SSH_PORT = 22
+PATH_TO_STREAM = f'http://{RASPI_PI_ETHERNET}:{str(RASPI_BROADCAST_PORT)}/?action=streaming'  # Wi-Fi Broadcast.
+# container's structure.
+DATASET_PATH = "/root/data"
+DATABASE_PATH = "/root/data/output/database.db"
+IMAGESET_PATH = "/root/data/dataset-m"
+OUTPUT_PATH = "/root/data/output"
+SPARSE_PATH = "/root/data/output/0"
+NEW_SPARSE_PATH = "/root/data/output/sparse"
+DENSE_PATH = "/root/data/output"
+DENSE_PLY_PATH = "/root/data/output/dense.ply"
 
 # create logger with 'spam_application'
 logging.getLogger('Raspi- application')
@@ -63,15 +88,15 @@ def set_working_directories(wd_path, debug_mode=False):
     dataset_m_name = 'dataset-m' + dataset_serial
     output_name = 'output' + dataset_serial
     path_data, path_data_m, path_output = tuple(
-                ''.join(i) for i in zip(tuple([data_lib_path]*3),
-                                        tuple((dataset_name, dataset_m_name, output_name))))
+        ''.join(i) for i in zip(tuple([data_lib_path] * 3),
+                                tuple((dataset_name, dataset_m_name, output_name))))
 
     if not debug_mode:  # create new directories.
         os.mkdir(path_data)
         os.mkdir(path_data_m)
         os.mkdir(path_output)
 
-    return path_data, path_data_m, path_output
+    return path_data, path_data_m, path_output, dataset_serial
 
 
 def SSH_open_camera(client, sharpness, brightness, contrast, fps, res_x, res_y, port=8080):
@@ -165,7 +190,7 @@ def mask_images(img_path, erode_func, output_lib, params, output_ext=".jpg"):
     img = cv2.imread(img_path)[vlc:vuc, hlc:hrc]  # cv2 read & crop
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # convert to hsv
     mask = cv2.inRange(hsv, (35, 25, 25), (80, 255, 255))  # mask by slicing the green spectrum
-    #mask = cv2.inRange(hsv, (0, 42, 0), (179, 255, 255)) # everything except white
+    # mask = cv2.inRange(hsv, (0, 42, 0), (179, 255, 255)) # everything except white
 
     # apply the masking:
     imask = mask > 0
@@ -204,53 +229,45 @@ def convert_images_to_masked(input_dir, output_dir, mask_func, erode_func, mask_
         mask_func(file, erode_func, output_dir, mask_params)
 
 
+def run_colmap(client, cmd_dict, mount_dict, wd):
+    """ dockerized COLMAP"""
+    for colmap_command, command in cmd_dict.items():
+        logging.info(f"executing colmap: {colmap_command}")
+        _execute_colmap_command(client, command, mount_dict, wd)
+
+
+def _execute_colmap_command(cl, cmd, mount_dict, wd, container_name='colmap:test'):
+    return cl.containers.run(container_name, cmd, volumes=mount_dict, working_dir=wd, runtime="nvidia", detach=False,
+                             auto_remove=True)
+
+
 def main():
-    """ raspi- plant. """
-
-    DEBUG_MODE = True
-
-    # set broadcasting parameters
-    RASPI_USER = "pi"
-    RASPI_PI_WIFI = "192.168.11.115"
-    RASPI_PI_ETHERNET = "10.150.180.52"
-    RASPI_PWD = 'Mancave3090!'
-    RASPI_BROADCAST_PORT = 8085
-    SSH_PORT = 22
-    path_to_stream = f'http://{RASPI_PI_ETHERNET}:{str(RASPI_BROADCAST_PORT)}/?action=streaming'  # Wi-Fi Broadcast.
-
-    parser = argparse.ArgumentParser(description="Raspi-plant")
-    parser.add_argument("-on", "--on", help="start streaming", nargs='+', type=int)
-    parser.add_argument("-off", "--off", help="end streaming", nargs='+', type=str)
-    parser.add_argument("-r", "--record", help="record a video", nargs='+', type=int)
-
-    parser.add_argument("-sa", "--algorithm",
-                        help="choose algorithm: False (default) for full search, True for LSH", nargs='+',
-                        type=bool, default=False)
-
-    args = parser.parse_args()
-
     # set working directories, input and output.
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    path_to_data_directory, path_to_masked_data_directory, path_to_output_dir = set_working_directories(dir_path, debug_mode=DEBUG_MODE)
+    path_to_data_directory, path_to_masked_data_directory, path_to_output_dir, serial = set_working_directories(
+        dir_path, debug_mode=FLAGS.debug)
 
-    # control
+    # logging control
     logging.debug(f"Path to the data directory: {path_to_data_directory}")
     logging.debug(f"Path to the masked-data directory: {path_to_masked_data_directory}")
     logging.debug(f"Path to output directory: {path_to_output_dir}")
 
     # delete content from existing directories.
-    # delete_data(path_to_data_directory)  # deletes content of the data library
-    # delete_data(path_to_masked_data_directory)  # deletes content of the masked data library
+    if FLAGS.delete:
+        delete_data(path_to_data_directory)  # deletes content of the data library
+        delete_data(path_to_masked_data_directory)  # deletes content of the masked data library
 
     # ssh connection
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(RASPI_PI_ETHERNET, SSH_PORT, RASPI_USER, RASPI_PWD)
 
-    """ create a video. """
-    #SSH_open_camera(ssh, sharpness=50, brightness=50, contrast=60, fps=2, res_x=1080, res_y=720, port=RASPI_BROADCAST_PORT)
-    #record_video(28, path_to_stream, path_to_data_directory)  # records a video
-    #SSH_shutdown_camera(ssh)
+    """ record a video. """
+    if FLAGS.record and FLAGS.record != 0:
+        SSH_open_camera(ssh, sharpness=50, brightness=50, contrast=60, fps=2, res_x=1080, res_y=720,
+                        port=RASPI_BROADCAST_PORT)
+        record_video(28, PATH_TO_STREAM, path_to_data_directory)  # records a video
+        SSH_shutdown_camera(ssh)
 
     logging.info(f"SSH transport is: {ssh.get_transport().active}")
     logging.info("closing SSH connection: ")
@@ -258,13 +275,57 @@ def main():
     logging.info("SSH transport is now closed")
 
     """ apply mask. """
-
-    mask_params = {"hlc": 0, "hrc": 800, "vlc": 0, "vuc": 580, "n": 4, "n_iter": 10}
-    convert_images_to_masked(path_to_data_directory, path_to_masked_data_directory, mask_images, erode_function,
+    if FLAGS.masking:
+        mask_params = {"hlc": 150, "hrc": 950, "vlc": 0, "vuc": 730, "n": 8, "n_iter": 20}
+        convert_images_to_masked(path_to_data_directory, path_to_masked_data_directory, mask_images, erode_function,
                              mask_params)
 
-    """ docker COLMAP"""
-    #client = docker.from_env()f
+    """colmap in docker"""
+    if FLAGS.colmap:
+        client = docker.from_env()  # connect to docker daemon
+
+        path_to_mount = path_to_data_directory
+        path_masked_to_mount = path_to_masked_data_directory
+        path_output_to_mount = path_to_output_dir
+
+        destination_mount = IMAGESET_PATH[:-2]
+        destination_masked_mount = IMAGESET_PATH
+        destination_output_mount = OUTPUT_PATH
+
+        mount_dict = {path_to_mount: {'bind': destination_mount, 'mode': 'rw'},
+                      path_masked_to_mount: {'bind': destination_masked_mount, 'mode': 'rw'},
+                      path_output_to_mount: {'bind': destination_output_mount, 'mode': 'rw'}}
+
+        CMD1 = f"colmap feature_extractor --database_path {DATABASE_PATH} --image_path {IMAGESET_PATH}"
+        CMD2 = f"colmap exhaustive_matcher \
+            --database_path {DATABASE_PATH}"
+        CMD3 = f"colmap mapper \
+            --database_path {DATABASE_PATH} \
+            --image_path {IMAGESET_PATH} \
+            --output_path {OUTPUT_PATH}"
+        CMD4 = f"colmap image_undistorter \
+            --image_path {IMAGESET_PATH} \
+            --input_path {SPARSE_PATH} \
+            --output_path {DENSE_PATH} \
+            --output_type COLMAP"
+        CMD5 = f"colmap patch_match_stereo \
+            --workspace_path {DENSE_PATH} \
+            --workspace_format COLMAP \
+            --PatchMatchStereo.geom_consistency true"
+        CMD6 = f"colmap stereo_fusion \
+            --workspace_path {DENSE_PATH} \
+            --workspace_format COLMAP \
+            --input_type geometric \
+            --output_path {DENSE_PLY_PATH}"
+        CMD7 = f"colmap model_converter \
+            --input_path {SPARSE_PATH} \
+            --output_path {SPARSE_PATH} \
+            --output_type TXT"
+
+        colmap_cmds = {"feature_extractor": CMD1, "exhaustive_matcher": CMD2, "mapper": CMD3, "image_undistorter": CMD4,
+                       "patch_match_stereo": CMD5, "stereo_fusion": CMD6, "model_converter": CMD7}
+
+        run_colmap(client, colmap_cmds, mount_dict, DATASET_PATH)
 
 
 if __name__ == "__main__":
